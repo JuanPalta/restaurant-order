20b4b1bff9a17b773e554809278d28a93a6b4544
Juan Palta <juanpalta24@hotmail.com>
2020-09-12 16:35:10 -0500

    class diagram with getters/setters

--- src/model/RestaurantAssociation.java
+++ src/model/RestaurantAssociation.java
@@ -1,12 +1,21 @@
 package model;
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ * @author JuanP
+ * 
+ * Class RestaurantAssociation
+ *
+ */
 public class RestaurantAssociation {
 	
 	private List<Restaurant> restaurants;
 	
 	public RestaurantAssociation() {
 		
-		
+		restaurants = new ArrayList<Restaurant>();
 	}
+	
 }
